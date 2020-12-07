89cd69065a397b6c41d0c8a9c5c0cb76a2fcdf2a
Rifqi Charisma <58881125+rifqicharisma@users.noreply.github.com>
2020-12-07 14:51:38 +0700

    click button and add activity
    menambahkan click pada register(textView) dan login (textView) lalu membuat notofikasi sukses registrasi

--- DesignRajaOngkir/app/src/main/java/com/example/designrajaongkir/MainActivity.java
+++ DesignRajaOngkir/app/src/main/java/com/example/designrajaongkir/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.designrajaongkir;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView registertext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // pindah main activity ke register acitvity
+        // dengan text view Register
+        registertext = (TextView) findViewById(R.id.registertext);
+        registertext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openRegisterActivity();
+            }
+        });
+    }
+
+    // open Register Acitivity
+    public void openRegisterActivity(){
+        Intent intent = new Intent(this, Register.class);
+        startActivity(intent);
     }
 }
--- DesignRajaOngkir/app/src/main/java/com/example/designrajaongkir/RegisterSuccess.java
+++ DesignRajaOngkir/app/src/main/java/com/example/designrajaongkir/RegisterSuccess.java
@@ -5,19 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
-public class Register extends AppCompatActivity {
+public class RegisterSuccess extends AppCompatActivity {
     private TextView textLoginInRegister;
-    private Button RegisterButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register);
+        setContentView(R.layout.activity_register_success);
 
-        // text Login Register click
+        // register text click
         textLoginInRegister = (TextView) findViewById(R.id.textLoginInRegister);
         textLoginInRegister.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,26 +23,11 @@ public class Register extends AppCompatActivity {
                 openLoginActivity();
             }
         });
-
-        // register button click
-        RegisterButton = (Button) findViewById(R.id.RegisterButton);
-        RegisterButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openRegisterSuccess();
-            }
-        });
     }
 
-    // open login acitivity
+    // open login
     public void openLoginActivity(){
         Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-    }
-
-    // open register success activity
-    public void openRegisterSuccess(){
-        Intent intent = new Intent(this, RegisterSuccess.class);
         startActivity(intent);
     }
 }
